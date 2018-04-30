ba6abb2bfd2d187c55483b73a51bb7d568745fef
ptliang <ptliang@hotmail.com>
2018-04-29 18:55:27 -0700

    update boiler plate to have SCSS and image support

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,34 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[chunkhash].js"
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/, // convert ES6 code to ES5 for compatibility
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/, // handle scss, convert to css and put in separate file
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/, // handle images, embed small ones
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 8000,
+            name: "images/[name]-[hash].[ext]"
+          }
         }
       }
     ]
@@ -16,6 +37,13 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.[contenthash].css",
+      chunkFilename: "[id].css"
+
+    }),
+    new CleanWebpackPlugin('dist', {} ) // delete dist folder each time build is made
+  ],
+  devtool: 'source-map'
 };
